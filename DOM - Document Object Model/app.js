3356473868911b642b95a0051b048c370727a0ed
Dhanushka <dhanushkamcr@gmail.com>
2025-07-27 12:49:06 -0500

    Move all javascript to js file

--- DOM - Document Object Model/app.js
+++ DOM - Document Object Model/app.js
@@ -1,27 +1,26 @@
-let number = 1;
+const boxContainer = document.getElementById('boxContainer');
+const btnCreateBox = document.getElementById('btnCreateBox');
+const btnRemoveBoxes = document.getElementById('btnRemoveBoxes');
 
-function createBox() {
-    const container = document.getElementById('container');
-    const box = document.createElement('div');
-    box.className = 'box';
-    const innerText = document.createElement('p');
-    innerText.textContent = number;
-    box.appendChild(innerText);
-
+function getRandomColor(){
     const maxVal = 0xFFFFFF; // Represents #FFFFFF
     let randomNumber = Math.floor(Math.random() * (maxVal + 1)); // +1 to include 0xFFFFFF
     let hexColor = randomNumber.toString(16);
     hexColor = hexColor.padStart(6, '0'); // Pad with leading zeros if needed
-    box.style.backgroundColor = '#' + hexColor.toUpperCase();
+    return '#' + hexColor.toUpperCase();
+}
 
-    container.appendChild(box);
-    number++;
+function createBox() {
+    const box = document.createElement("div");
+    box.classList.add("box");
+    box.innerText = document.querySelectorAll(".box").length + 1;
+    box.style.backgroundColor = getRandomColor();
+    boxContainer.appendChild(box);
 }
 
 function removeAllBoxes() {
-    const container = document.getElementById('container');
-    container.innerHTML = '';
-    number = 1;
+    boxContainer.innerHTML = "";
 }
 
-
+btnCreateBox.addEventListener("click", createBox);
+btnRemoveBoxes.addEventListener("click", removeAllBoxes);
