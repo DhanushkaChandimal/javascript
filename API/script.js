7ca212f4e367722c0466b97b8d0bc74a85de12e2
Dhanushka <dhanushkamcr@gmail.com>
2025-07-28 05:56:52 -0500

    javascript API

---
+++ API/script.js
@@ -0,0 +1,4 @@
+const singleDogButton = document.getElementById("single-dog-button");
+const multipleDogButton = document.getElementById("multiple-dog-button");
+const singleDogContainer = document.getElementById("single-dog-container");
+const multipleDogContainer = document.getElementById("multiple-dog-container");
